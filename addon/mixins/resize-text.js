3c96375d557b8c86e8444fe4fe573d5e53b5e093
Tom Zellman <tzellman@gmail.com>
2018-08-03 18:10:26 -0400

    - use the computed style width, which is the actual inner width - subract padding - only set the element font-size iff it changed - add shrink/grow shortcuts for not having to know min/max pixels - pass the current font size to the textMeasurer - use the container style properties

--- addon/mixins/resize-text.js
+++ addon/mixins/resize-text.js
@@ -1,12 +1,30 @@
 import Mixin from '@ember/object/mixin';
 import { inject as service } from '@ember/service';
 
+const extractPixels = function(value) {
+  let match = /(\d+)px/i.exec(value || '');
+  return match ? parseInt(match[1], 10) : 0;
+};
+
 export default Mixin.create({
 
   classNames: ['resize-text'],
 
   minSize: 2,
   maxSize: 80,
+
+  // NOTE: shrink and grow are shortcuts (as mentioned below)
+  // the reasoning: often times the template developer does not have access to (or does not know)
+  // the specified font size (in pixels)
+  // obviously that can be computed by you, the developer, but these shortcuts handle it for you
+
+  // passed-in: set to false if you only want to grow the text (never shrink)
+  // setting to false is an alternative to setting maxSize to your current font size
+  shrink: true,
+
+  // passed-in: set to false if you only want to shrink the text (never grow)
+  // setting to false is an alternative to setting minSize to your current font size
+  grow: true,
 
   textMeasurer: service(),
 
@@ -23,24 +41,34 @@ export default Mixin.create({
   },
 
   scaleFont() {
-    const style = getComputedStyle(this.element);
-    const minSize = this.get('minSize');
-    const maxSize = this.get('maxSize');
-    const container = this.get('containerElement');
-    let elementWidth;
-    if (container) {
-      elementWidth = container.clientWidth;
-    } else {
-      elementWidth = this.element.clientWidth;
-    }
+    const container = this.get('containerElement') || this.element;
+    const style = getComputedStyle(container);
+    const currentFontSize = extractPixels(style.fontSize);
+
+    // if shrink or grow are set to false, use the currentFontSize
+    const { shrink, grow } = this.getProperties('shrink', 'grow');
+    const minSize = !shrink ? currentFontSize : this.get('minSize');
+    const maxSize = !grow ? currentFontSize : this.get('maxSize');
+
+    // the computed style.width is the given width, w/o padding, which is what we want
+    let elementWidth = extractPixels(style.width) || container.clientWidth;
+    // remove the padding, which is the actual inner/renderable width
+    elementWidth -= extractPixels(style.paddingLeft);
+    elementWidth -= extractPixels(style.paddingRight);
+
     let fontSize = this.get('textMeasurer')
-        .fitTextSize(this.element.innerText, elementWidth, style.fontStyle + ' 14px ' + style.fontFamily);
+        .fitTextSize(this.element.innerText, elementWidth,
+          `${style.fontStyle} ${style.fontSize} ${style.fontFamily}`);
     if (fontSize > maxSize) {
       fontSize = maxSize;
     } else if (fontSize < minSize) {
       fontSize = minSize;
     }
-    this.element.style['font-size'] = fontSize + 'px';
+
+    // only set the style if it actually changed
+    if (fontSize !== currentFontSize) {
+        this.element.style['font-size'] = fontSize + 'px';
+    }
   },
 
   didUpdate() {
--- tests/integration/components/resize-text-test.js
+++ tests/integration/components/resize-text-test.js
@@ -71,4 +71,67 @@ module('Integration | Component | resize text', function(hooks) {
     assert.dom('.resize-text').hasText('template block text');
     assert.equal(getComputedStyle(find('.resize-text'))['font-size'], '20px');
   });
+
+  test('do not grow if we are told not to', async function(assert) {
+
+    // in some situations you may want to only shrink the text if it can't fit in the container
+
+    getRootElement().getElementsByClassName('ember-view')[0].style.width = '800px';
+    getRootElement().style.fontSize = '10px';
+
+    await render(hbs`
+      {{#resize-text grow=false}}
+        template block text
+      {{/resize-text}}
+    `);
+
+    assert.dom('.resize-text').hasText('template block text');
+    assert.equal(getComputedStyle(find('.resize-text'))['font-size'], '10px');
+  });
+
+  test('do not shrink if we are told not to', async function(assert) {
+
+    getRootElement().getElementsByClassName('ember-view')[0].style.width = '100px';
+    getRootElement().style.fontSize = '20px';
+
+    await render(hbs`
+      {{#resize-text shrink=false}}
+        template block text
+      {{/resize-text}}
+    `);
+
+    assert.dom('.resize-text').hasText('template block text');
+    assert.equal(getComputedStyle(find('.resize-text'))['font-size'], '20px');
+  });
+
+  test('resize with padding taken into account', async function(assert) {
+
+    // first compute without padding
+    let container = getRootElement();
+    container.style.width = '100px';
+    container.style['font-size'] = '10px';
+    this.set('containerElement', container);
+    await render(hbs`
+      {{#resize-text containerElement=containerElement}}
+        template block text
+      {{/resize-text}}
+    `);
+    assert.dom('.resize-text').hasText('template block text');
+    assert.equal(getComputedStyle(find('.resize-text'))['font-size'], '13px');
+
+    // now, adding padding - we increase the width of the container, but add padding
+    container = getRootElement();
+    container.style.width = '140px';
+    container.style['font-size'] = '10px';
+    container.style.paddingLeft = '20px';
+    container.style.paddingRight = '20px';
+    this.set('containerElement', container);
+    await render(hbs`
+      {{#resize-text containerElement=containerElement}}
+        template block text
+      {{/resize-text}}
+    `);
+    assert.dom('.resize-text').hasText('template block text');
+    assert.equal(getComputedStyle(find('.resize-text'))['font-size'], '13px');
+  });
 });
